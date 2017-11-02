e0df500ca07be84e49d4ca440481972208eb7c89
cm1 <chowdappa_m@intuit.com>
2017-11-02 15:07:15 +0530

    initial commit

---
+++ zip_demo.py
@@ -0,0 +1,16 @@
+first = ["john", "lucy", "haidi"]
+last = ["Rob", "tuna", "devil"]
+
+names = zip(first, last)
+
+for a, b in names:
+    print (a, b)
+
+a1 = [1,2,3,4]
+a2 = [10,20, 30,40]
+a3 = ["x","y","z", "U"]
+
+matrix = zip(a1,a2,a3)
+
+for a,b,c in matrix:
+    print (a, b,c)
